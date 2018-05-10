e3b39ec25cdef64d71ccb5f0ab5c9dc11af7f869
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-10 11:52:20 +0300

    added css and bootstrap

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'photogall',
     'tinymce',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
